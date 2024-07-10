01c3976a37afb2e5e6ed5750a4c086fdd2e21097
Ionela Alexandra <63660313+ionero12@users.noreply.github.com>
2024-07-10 23:27:10 +0300

    basic react files

---
+++ RecipesWebsite_Frontend/my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import Album from './components/Album';
+import RecipeDetail from './components/RecipeDetail';
+import RecipeByIngredients from "./components/RecipeByIngredients";
+
+function App() {
+    return (<Router>
+        <Routes>
+            <Route path="/home" exact element={<Album/>} />
+            <Route path="/recipe/:idRecipe" element={<RecipeDetail/>} />
+            <Route path="/ingredients" element={<RecipeByIngredients/>} />
+            <Route path="*" element={<Navigate to="/home"/>}/>
+        </Routes>
+    </Router>);
+}
+
+export default App;
