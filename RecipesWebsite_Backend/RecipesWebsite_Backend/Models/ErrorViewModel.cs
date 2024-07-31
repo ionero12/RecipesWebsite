e27e9f17c32d4e873ede405f57ba1ddb099d7bf0
ionero12 <sauionela51@gmail.com>
2024-07-31 22:23:50 +0300

    build stuff

--- RecipesWebsite_Backend/RecipesWebsite_Backend/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RecipesWebsite_Backend.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
